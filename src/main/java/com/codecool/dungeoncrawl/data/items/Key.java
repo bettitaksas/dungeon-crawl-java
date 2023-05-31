2afece7b8d10d9b4bf345b9cc927abaf3a115966
szigetvarikati <szigetvarikati@gmail.com>
2023-05-31 11:00:54 +0200

    create key class use item interface

---
+++ src/main/java/com/codecool/dungeoncrawl/data/items/Key.java
@@ -0,0 +1,13 @@
+package com.codecool.dungeoncrawl.data.items;
+
+import com.codecool.dungeoncrawl.data.Cell;
+
+public class Key extends Item{
+    public Key(Cell cell) {
+        super(cell);
+    }
+    @Override
+    public String getTileName() {
+        return "goldenKey";
+    }
+}
